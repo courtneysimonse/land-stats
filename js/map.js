057f64e43425b65b2c4625193440d1b8d5327215
Courtney Simonse <courtney@artemismaps.com>
2024-05-03 10:21:26 -0400

    add zoom to state

--- js/map.js
+++ js/map.js
@@ -8,6 +8,134 @@ const map = new mapboxgl.Map({
     projection: 'mercator'
 });
 
+
+let selectedState;
+let selectedStateId;
+let selectedCounty;
+let selectedCountyId;
+let hoverState;
+let hoverCounty;
+
+let filters = {};
+
 map.on('load', () => {
-    
+
+    const defaultStateColors = map.getPaintProperty('states-totals', 'fill-color');
+    // // Add the control to the map.
+    // map.addControl(
+    //     new MapboxGeocoder({
+    //         accessToken: mapboxgl.accessToken,
+    //         mapboxgl: mapboxgl,
+    //         localGeocoder: forwardGeocoder
+    //     })
+    // );
+
+    // function forwardGeocoder(query) {
+    //     var matchingFeatures = [];
+    //     for (var i = 0; i < customData.features.length; i++) {
+    //         var feature = customData.features[i];
+    //         // handle queries with different capitalization than the source data by calling toLowerCase()
+    //         if (
+    //             feature.properties.title
+    //                 .toLowerCase()
+    //                 .search(query.toLowerCase()) !== -1
+    //         ) {
+    //             // add a tree emoji as a prefix for custom data results
+    //             // using carmen geojson format: https://github.com/mapbox/carmen/blob/master/carmen-geojson.md
+    //             feature['place_name'] = '🌲 ' + feature.properties.title;
+    //             feature['center'] = feature.geometry.coordinates;
+    //             feature['place_type'] = ['park'];
+    //             matchingFeatures.push(feature);
+    //         }
+    //     }
+    //     return matchingFeatures;
+    // }
+
+    map.on('zoomend', () => {
+        if (map.getZoom() >= 9 || selectedCounty != null) {
+            map.setLayoutProperty('zip-totals-Zoom 5', 'visibility', 'visible');
+        } else if (map.getZoom() >= 5 || selectedState != null) {
+            map.setLayoutProperty('counties-totals', 'visibility', 'visible');
+            map.setLayoutProperty('zip-totals-Zoom 5', 'visibility', 'none');
+        } else {
+            map.setLayoutProperty('counties-totals', 'visibility', 'none');
+        }
+
+    });
+
+    map.on('mouseenter', ['states-totals', 'counties-totals', 'zip-totals-Zoom 5'], (e) => {
+        map.getCanvas().style.cursor = 'pointer';
+    })
+
+    map.on('mouseleave', ['states-totals', 'counties-totals', 'zip-totals-Zoom 5'], (e) => {
+        map.getCanvas().style.cursor = '';
+    })
+
+
+    // When a click event occurs on a feature in the states layer, zoom to bounds
+    // of the feature and display congressional districts and top line stats
+    map.on('click', ['states-totals', 'counties-totals'], (e) => {
+
+        if (e.features[0].layer.id == 'states-totals') {
+            selectedCounty = null;
+            map.fitBounds(e.features[0].properties.bbox, {padding: 50});
+
+            selectedStateId = e.features[0].id;
+        } else {
+
+            map.setPaintProperty('states-totals', 'fill-color', [
+                'case',
+                ['==', ['get', 'GEOID'], e.features[0].properties['GEOID']], defaultStateColors,
+                'grey'
+            ]);
+
+
+            if (selectedCountyId) {
+
+                map.setFeatureState(
+                    {
+                        source: 'composite',
+                        sourceLayer: 'counties-totals',
+                        id: selectedCountyId
+                    },
+                    { selected: false }
+                );
+
+            }
+
+            map.setFeatureState(
+                {
+                    source: 'composite',
+                    sourceLayer: 'counties-totals',
+                    id: e.features[0].id
+                },
+                { selected: true }
+            );
+
+            selectedCountyId = e.features[0].id;
+
+        }
+
+
+    });
+
+
 })
+
+// Because features come from tiled vector data,
+// feature geometries may be split
+// or duplicated across tile boundaries.
+// As a result, features may appear
+// multiple times in query results.
+function getUniqueFeatures(features, comparatorProperty) {
+    const uniqueIds = new Set();
+    const uniqueFeatures = [];
+    for (const feature of features) {
+        const id = feature.properties[comparatorProperty];
+        if (!uniqueIds.has(id)) {
+            uniqueIds.add(id);
+            uniqueFeatures.push(feature);
+        }
+    }
+    return uniqueFeatures;
+}
