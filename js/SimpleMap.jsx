4f801ff2a262cebf93cd7bfea69ce3f40e05a4da
courtneysimonse <courtney@artemismaps.com>
2024-06-25 17:13:47 -0400

    remove unecessary files

--- js/SimpleMap.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useEffect, useState, useRef, useCallback } from "react";
-import * as d3 from "d3-fetch";
-import mapboxgl from "mapbox-gl";
-
-import 'mapbox-gl/dist/mapbox-gl.css';
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
-
-import "./MapComponent.css";
