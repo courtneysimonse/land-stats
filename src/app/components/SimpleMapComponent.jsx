1d83905b97798dd1881b5f9517159e8a7fd582cb
Courtney Simonse <courtney@artemismaps.com>
2024-07-01 15:05:02 -0400

    add hover colors

--- src/app/components/SimpleMapComponent.jsx
+++ src/app/components/SimpleMapComponent.jsx
@@ -22,32 +22,24 @@ const SimpleMapComponent = () => {
   const mapContainer = useRef(null);
   const map = useRef(null);
   const [layer, setLayer] = useState("State");
-  const [hoveredPolygonId, setHoveredPolygonId] = useState([]);
+  const hoveredPolygonId = useRef([]);
   const [states, setStates] = useState(null);
   const [counties, setCounties] = useState(null);
 
   useEffect(() => {
     const loadData = async () => {
       // load CSVs
-      const states = await d3.csv('./data/states.csv');
-      const counties = await d3.csv('./data/counties.csv');
-      setStates(states);
-      setCounties(counties);
+      const statesData = await d3.csv('./data/states.csv');
+      const countiesData = await d3.csv('./data/counties.csv');
+      setStates(statesData);
+      setCounties(countiesData);
     }
     loadData()
   }, []);
 
-
-  function createPopup(feature) {
-
-    let popupContent = document.createElement('h3');
-    let data = feature.layer.id === "states-fill" ? states : counties;
-    popupContent.innerText = data.find(x => x["GEOID"] == feature.properties["GEOID"]).NAME
-
-    return popupContent;
-  }
-
   useEffect(() => {
+    if (!states || !counties) return; // Wait for the data to load
+
     if (map.current) return; // initialize map only once
     mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 
@@ -58,25 +50,145 @@ const SimpleMapComponent = () => {
       projection: 'mercator'
     });
 
-  }, []);
+    const createPopup = (feature) => {
 
-  useEffect(() => {
+      let popupContent = document.createElement('h3');
+      let data = feature.layer.id === "states-fill" ? states : counties;
+      popupContent.innerText = data?.find(x => +x["GEOID"] === feature.id)?.NAME || '';
+      return popupContent;
 
-    if (map.current) return; // make sure map is initialized
+    }
+
 
     const tooltip = new mapboxgl.Popup({ closeButton: false, className: 'simple-tooltip' });
 
-    map.current.on('mouseenter', ['states-fill', 'counties'], () => {
-      map.current.getCanvas().style.cursor = 'pointer';
-      
-    });
 
-    map.current.on('mousemove', ['states-fill', 'counties'], (e) => {
-      let popupContent = createPopup(e.features[0]);
+    map.current.on('load', () => {
 
-      tooltip.setHTML(popupContent.outerHTML)
-        .setLngLat(e.lngLat)
-        .addTo(map.current);
+      map.current.setPaintProperty('counties', 'fill-color', 
+        [
+          "case",
+          [
+            "boolean",
+            [
+              "feature-state",
+              "hover"
+            ],
+            false
+          ],
+          "#ffe168",
+          "#48ba2d"
+        ]
+      )
+
+      map.current.setPaintProperty('counties', 'fill-opacity',
+        [
+          "case",
+          [
+            "boolean",
+            [
+              "feature-state",
+              "selected"
+            ],
+            true
+          ],
+          1,
+          0.4
+        ]
+      )
+  
+      map.current.on('mouseenter', ['states-fill', 'counties'], () => {
+        map.current.getCanvas().style.cursor = 'pointer';
+        
+      });
+  
+      map.current.on('mousemove', ['states-fill', 'counties'], (e) => {
+        if (e.features.length === 0) return;
+
+        // Clear the previously hovered polygon states
+        if (hoveredPolygonId.current.length > 0) {
+          hoveredPolygonId.current.forEach(id => {
+            map.current.setFeatureState(
+              {
+                source: 'composite',
+                sourceLayer: 'counties',
+                id: id
+              },
+              { hover: false }
+            );
+          });
+        }
+        
+        let popupContent = createPopup(e.features[0]);
+  
+        tooltip.setHTML(popupContent.outerHTML)
+          .setLngLat(e.lngLat)
+          .addTo(map.current);
+  
+        if (e.features.length > 0) {
+          const counties = map.current.queryRenderedFeatures(
+            {
+              filter: ["==", ["to-number", ["get", "ST_GEOID"]], e.features[0].id],
+              layers: ["counties"]
+            }
+          );
+  
+          const newHoveredPolygonId = [];
+
+          counties.forEach(county => {
+            map.current.setFeatureState(
+              {
+                source: 'composite',
+                sourceLayer: 'counties',
+                id: county.id
+              },
+              { hover: true }
+            );
+            newHoveredPolygonId.push(county.id); // Collect the new IDs for the hover state
+          });
+        
+          hoveredPolygonId.current = newHoveredPolygonId; // Update the state with the new IDs
+  
+        }
+  
+        // console.log(e);
+  
+      });
+  
+      map.current.on('mouseleave', ['states-fill', 'counties'], () => {
+        map.current.getCanvas().style.cursor = '';
+        tooltip.remove();
+      });
+  
+      // const popup = new mapboxgl.Popup({ closeButton: true, className: 'simple-tooltip' });
+      map.current.on('click', ['states-fill'], (e) => {
+        // tooltip.remove();
+  
+        // let popupContent = createPopup(e.features[0]);
+  
+        // popup.setHTML(popupContent.outerHTML)
+        //   .setLngLat(e.lngLat)
+        //   .addTo(map.current)
+  
+        map.current.fitBounds(e.features[0].properties.bbox.split(","), 
+        {padding: 200});  
+
+        // Clear the previously hovered polygon states
+        if (hoveredPolygonId.current.length > 0) {
+        hoveredPolygonId.current.forEach(id => {
+          map.current.setFeatureState(
+            {
+              source: 'composite',
+              sourceLayer: 'counties',
+              id: id
+            },
+            { 
+              hover: false,
+              selected: false
+            }
+          );
+        });
+      }
 
       if (e.features.length > 0) {
         const counties = map.current.queryRenderedFeatures(
@@ -86,69 +198,39 @@ const SimpleMapComponent = () => {
           }
         );
 
+        const newHoveredPolygonId = [];
+
         counties.forEach(county => {
-
-          // if (hoveredPolygonId.length > 0) {
-          //   map.current.setFeatureState(
-          //       { source: 'composite', 
-          //         sourceLayer: 'albers-composite-us-counties-3ylana',
-          //         id: hoveredPolygonId },
-          //       { hover: false }
-          //   );
-          // }
-          setHoveredPolygonId(prev => {
-            if (!prev.includes(county.id)) {
-              return [...prev, county.id];
-            }
-            return prev;
-          });
           map.current.setFeatureState(
-              { source: 'composite', 
-                sourceLayer: 'counties',
-                id: county.id },
-              { hover: true }
+            {
+              source: 'composite',
+              sourceLayer: 'counties',
+              id: county.id
+            },
+            { hover: true }
           );
-          
+          newHoveredPolygonId.push(county.id); // Collect the new IDs for the hover state
         });
-        
-
+      
+        hoveredPolygonId.current = newHoveredPolygonId; // Update the state with the new IDs
 
       }
-
-      // console.log(e);
-
-    });
-
-    map.current.on('mouseleave', ['states-fill', 'counties'], () => {
-      map.current.getCanvas().style.cursor = '';
-      tooltip.remove();
-    });
-
-    // const popup = new mapboxgl.Popup({ closeButton: true, className: 'simple-tooltip' });
-    map.current.on('click', ['states-fill', 'counties'], (e) => {
-      tooltip.remove();
-
-      // let popupContent = createPopup(e.features[0]);
-
-      // popup.setHTML(popupContent.outerHTML)
-      //   .setLngLat(e.lngLat)
-      //   .addTo(map.current)
-
-      map.current.fitBounds(e.features[0].properties.bbox.split(","), 
-        {padding: 200});
-
-      map.current.setFeatureState({
-        source: 'composite',
-        sourceLayer: 'counties',
-        id: e.features[0].id,
-      }, {
-          selected: true,
-          hover: false
+  
+        map.current.setFeatureState({
+          source: 'composite',
+          sourceLayer: 'counties',
+          id: e.features[0].id,
+        }, {
+            selected: true,
+            hover: false
+        });
+  
+        map.current.setFilter('states-fill', ["!=", ["id"], e.features[0].id])
+        // map.current.setLayoutProperty('states-fill', 'visibility', 'none');
+  
       });
-
-      map.current.setLayoutProperty('states-fill', 'visibility', 'none');
-
-    });
+    })
+    
 
     return () => {
       if (map.current) {
@@ -158,7 +240,8 @@ const SimpleMapComponent = () => {
         map.current.off('click');
       }
     };
-  }, [states, counties])
+
+  }, [states, counties]);
 
   return (
     <div style={{ height: "100%", width: "100%" }}>
