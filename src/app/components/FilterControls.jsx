f4d5db4bf7cb9aaaa7a0cd2be8c6aec7a818a0f7
Courtney Simonse <courtney@artemismaps.com>
2024-12-05 12:01:25 -0500

    move filter controls to separate file

---
+++ src/app/components/FilterControls.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import config from "./mapConfig";
+
+const FilterControls = ({ filters, handleSelectChange, filterConfigs }) => {
+  return (
+    <div id="map-filters">
+        <fieldset>
+        {filterConfigs.map(({ label, name, options }) => {
+            const selectOptions = name === "stat" ? config.statOptions[filters.status] : options;
+
+            return (
+                <div key={name} className="filter-group">
+                <label htmlFor={`${name}-select`}>{label}:</label>
+                <select
+                    id={`${name}-select`}
+                    name={name}
+                    value={filters[name]}
+                    onChange={handleSelectChange}
+                >
+                    {name === "stat" 
+                    ? Object.entries(selectOptions || {}).map(([optionLabel, optionValue]) => (
+                        <option key={optionValue || optionLabel} value={optionValue || optionLabel}>
+                            {optionLabel}
+                        </option>
+                        ))
+                    : options.map(option => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                        ))
+                    }
+                </select>
+                </div>
+            );
+            })}
+        </fieldset>
+    </div>
+  );
+};
+
+export default FilterControls;
--- src/app/components/MapComponent.jsx
+++ src/app/components/MapComponent.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef, useCallback } from "react";
 import * as d3 from "d3-fetch";
 import LegendControl from "./LegendControl";
 import ZoomDisplayControl from "./ZoomDisplayControl";
+import FilterControls from "./FilterControls";
 import mapboxgl from "mapbox-gl";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 import config from "./mapConfig";
@@ -311,38 +312,11 @@ const MapComponent = () => {
         // flexWrap: "wrap",
         justifyContent: "center"
       }}>
-      <div id="map-filters">
-        <fieldset>
-          {filterConfigs.map(({ label, name, options }) => {
-              const selectOptions = name === "stat" ? config.statOptions[filters.status] : options;
-
-              return (
-                <div key={name} className="filter-group">
-                  <label htmlFor={`${name}-select`}>{label}:</label>
-                  <select
-                    id={`${name}-select`}
-                    name={name}
-                    value={filters[name]}
-                    onChange={handleSelectChange}
-                  >
-                    {name === "stat" 
-                      ? Object.entries(selectOptions || {}).map(([optionLabel, optionValue]) => (
-                          <option key={optionValue || optionLabel} value={optionValue || optionLabel}>
-                            {optionLabel}
-                          </option>
-                        ))
-                      : options.map(option => (
-                          <option key={option} value={option}>
-                            {option}
-                          </option>
-                        ))
-                    }
-                  </select>
-                </div>
-              );
-            })}
-        </fieldset>
-      </div>
+      <FilterControls
+        filters={filters}
+        handleSelectChange={handleSelectChange}
+        filterConfigs={filterConfigs}
+      />
       <div id="map"
         ref={mapContainer}
         style={{
